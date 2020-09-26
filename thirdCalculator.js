5b19e958e1f26e60ad0fe1228382b47fb573223f
david <david.mosca69@gmail.com>
2020-09-26 14:23:40 +0200

    modify some bugs after external merge

--- thirdCalculator.js
+++ thirdCalculator.js
@@ -1,4 +1,4 @@
-let result = document.querySelector("#result");
+let displayScreen = document.querySelector("#displayScreen");
 let decimalButton = document.querySelector("#btnVirgule");
 let calculateButton = document.querySelector("#btnCalculate");
 let resetButton = document.querySelector(".reset");
@@ -14,70 +14,90 @@ document.querySelector('#form').addEventListener('submit', function (e) {
 
 // effects mousedown & mouseup on buttons
 let btns = document.querySelectorAll('.btn');
-for (let i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++)
+{
     btns[i].addEventListener("mousedown", function(e) {
         btns[i].style.backgroundColor = "white";
     })
     btns[i].addEventListener("mouseup", function(e) {
-        btns[i].style.backgroundColor = "rgba(0, 0, 0, 0)";
+        //btns[i].style.backgroundColor = "rgba(0, 0, 0, 0)";
+        // ** indogo because we need same color as default when mouseup **
+        btns[i].style.backgroundColor = "indigo";
     })  
 }
 
 // add events on calculators buttons for write in the "screen"
-for(let i = 0; i<=9; i++){
+for(let i = 0; i<=9; i++)
+{
     let btn = document.querySelector("#btn"+i)
 
     btn.addEventListener("click", function(e) {
-        result.value += i;
+        displayScreen.value += i;
     })
 }
 
 // Add event on "virgule" button
 decimalButton.addEventListener("click", function(e) {
-    result.value += ".";
+    // if screen contain '.' we don't add one
+    if (displayScreen.value.indexOf(".") === -1)
+    {
+        displayScreen.value += ".";
+    }
     //prevents from entering '.' twice (.disabled didn't work)
-    decimalButton.style.display = "none";
-    calculateButton.style.gridColumn = "3/5";
+
+    /* with this code under, we can't enter second value with "." because the button desapear and need to click
+    on reset for display it again.*/
+    //decimalButton.style.display = "none";
+    //calculateButton.style.gridColumn = "3/5";
 })
 
 // store numbers when user push operator button
 let operators = document.querySelectorAll('.operator')
 
-for (let i = 0; i < operators.length; i++) {
+for (let i = 0; i < operators.length; i++)
+{
     operators[i].addEventListener("click", function (e) {
-        if(!value1) {
-            value1 = +result.value;
-            operand = operators[i].textContent;
-            result.value = "";
+        if(value1)
+        {
+            value1 = +displayScreen.value;
+            displayScreen.value = "";
+            console.log(operators[i]);
+            operand = operators[i].nodeValue[1].textContent;
             //console.log(`value1 = ${ value1 }`);
-        } else if(!value2) {
-            value2 = +result.value;
-            result.value = "";
-            //console.log(`value2 = ${ value2 }`);
+            if(value2)
+            {
+                value2 = +displayScreen.value;
+                displayScreen.value = "";
+                //console.log(`value2 = ${ value2 }`);
+            }
         }
     })
 }
 
 // On calcule !
-calculateButton.addEventListener('click', function(e) {
-    if (value1) {
+btnCalculate.addEventListener('click', function(e) {
+    if (value1)
+    {
         value1 = +value1;
-        if (!value2) {
-            value2 = +result.value
+
+        if (!value2)
+        {
+            value2 = +displayScreen.value
         }
+
         //console.log(`value1 : ${value1} ${typeof(value1)} - value2 : ${value2} ${typeof(value1)}`)
         switch(operand){
             case '+':
-                result.value =  +value1 + +value2;
+                displayScreen.value =  +value1 + +value2;
                 break;
             case '-':
-                result.value = +value1 - +value2;
+                displayScreen.value = +value1 - +value2;
                 break;
             case 'x':
-                result.value = +value1 * +value2;
+                displayScreen.value = +value1 * +value2;
                 break;
             case '/':
-                result.value = +value1 / +value2;
+                displayScreen.value = +value1 / +value2;
                 break;
             default:
                 throw new Error("Invalid operator");
@@ -85,15 +105,16 @@ calculateButton.addEventListener('click', function(e) {
         value1 = null;
         value2 = null;
         operand = null;
-      }
+    }
 })
 
 //Resets everything (couldn't use a button type="reset" because of bootstrap)
-resetButton.addEventListener('click', () => {
+btnReset.addEventListener('click', () => {
     value1 = null;
     value2 = null;
     operand = null;
-    result.value = "";
-    decimalButton.style.display = "flex";
-    calculateButton.style.gridColumn = "4/5";
+    // already implemented in reset button form (reset all inputs of the form)
+    //displayScreen.value = "";
+    //decimalButton.style.display = "flex";
+    //calculateButton.style.gridColumn = "4/5";
 })
